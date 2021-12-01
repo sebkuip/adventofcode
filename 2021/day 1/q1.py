676ebd01646a975028afb19007c5d4b7a9537047
sebkuip <kuipers.sebastian@ziggo.nl>
2021-12-01 13:59:52 +0100

    added comments

--- 2021/day 1/q1.py
+++ 2021/day 1/q1.py
@@ -4,8 +4,10 @@ with open('input.txt', 'r') as f:
 counter = 0
 
 for i in range(len(data)):
+    # break at the end of the dataset
     if i == len(data) - 1:
         break
+    # check if the next number is larger than the current number
     if data[i] < data[i + 1]:
         counter += 1
 
--- 2021/day 1/q2.py
+++ 2021/day 1/q2.py
@@ -5,8 +5,10 @@ counter = 0
 
 
 for i in range(len(data)):
+    # break at the end of the dataset
     if i == len(data) - 3:
         break
+    # check if the next numberset is larger than the current numberset
     if data[i] + data[i+1] + data[i+2] < data[i+1] + data[i+2] + data[i+3]:
         counter += 1
 
