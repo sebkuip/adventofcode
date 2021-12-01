1246d0a17f1a863b1f4f99ff316a1e5556a577c0
sebkuip <kuipers.sebastian@ziggo.nl>
2021-12-01 14:10:03 +0100

    Refined answers

--- 2021/day 1/q1.py
+++ 2021/day 1/q1.py
@@ -1,14 +1,6 @@
 with open('input.txt', 'r') as f:
     data = [int(line.strip()) for line in f]
 
-counter = 0
-
-for i in range(len(data)):
-    # break at the end of the dataset
-    if i == len(data) - 1:
-        break
-    # check if the next number is larger than the current number
-    if data[i] < data[i + 1]:
-        counter += 1
+counter = sum(1 for i in range(len(data) - 1) if data[i] < data[i + 1])
 
 print(counter)
--- 2021/day 1/q2.py
+++ 2021/day 1/q2.py
@@ -1,15 +1,6 @@
 with open('input.txt', 'r') as f:
     data = [int(line.strip()) for line in f]
 
-counter = 0
-
-
-for i in range(len(data)):
-    # break at the end of the dataset
-    if i == len(data) - 3:
-        break
-    # check if the next numberset is larger than the current numberset
-    if data[i] + data[i+1] + data[i+2] < data[i+1] + data[i+2] + data[i+3]:
-        counter += 1
+counter = sum(1 for i in range(len(data) - 3) if data[i] + data[i+1] + data[i+2] < data[i+1] + data[i+2] + data[i+3])
 
 print(counter)
