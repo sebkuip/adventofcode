2118767e673745f2f0768a818dac57cbc17a1a80
sebkuip <kuipers.sebastian@ziggo.nl>
2021-12-01 14:16:09 +0100

    More refinement

--- 2021/day 1/q1.py
+++ 2021/day 1/q1.py
@@ -1,6 +1,6 @@
 with open('input.txt', 'r') as f:
     data = [int(line.strip()) for line in f]
 
-counter = sum(1 for i in range(len(data) - 1) if data[i] < data[i + 1])
+counter = sum(data[i] < data[i + 1] for i in range(len(data) - 1))
 
 print(counter)
--- 2021/day 1/q2.py
+++ 2021/day 1/q2.py
@@ -1,6 +1,6 @@
 with open('input.txt', 'r') as f:
     data = [int(line.strip()) for line in f]
 
-counter = sum(1 for i in range(len(data) - 3) if data[i] + data[i+1] + data[i+2] < data[i+1] + data[i+2] + data[i+3])
+counter = sum(data[i] + data[i+1] + data[i+2] < data[i+1] + data[i+2] + data[i+3] for i in range(len(data) - 3))
 
 print(counter)
